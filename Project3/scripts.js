6a8217586c7f9d901b7a7fbba6f27805b21cc06a
Jize Tao <JasonTao@Jasons-MacBook-Air.local>
2018-03-27 23:38:43 +0400

    updated

--- Project3/scripts.js
+++ Project3/scripts.js
@@ -1,26 +1,10 @@
 function begin() {
-	window.scrollTo(0, 1020);
+	window.scrollTo(0, 786);
 }
 function back() {
 	window.scrollTo(0, 0);
 }
-/*after click the icon, the screen goes down and plane audio */
-function one() {
-	window.scrollTo(0, 1850);
-	var audio = document.getElementById("plane");
-	audio.play();
-
-	
-}
-
-function two() {
-	window.scrollTo(0, 2685);
-	var audio = document.getElementById("plane");
-	audio.pause();
-
-}
-function three() {
-	window.scrollTo(0, 3529);
+function climb() {
 	var elem = document.getElementById("man");   
 	  var pos = 28.53;
 	  var id = setInterval(frame, 200);
@@ -31,15 +15,33 @@ function three() {
 	    } else {
 	      pos++; 
 	      elem.style.left = 0.34*pos + 'vw'; 
-	      elem.style.top = 528.53 - pos + 'vh'; 
+	      elem.style.top = 528.53-50 - pos + 'vh'; 
 	      document.getElementById("man").style.animation = "shake 2s infinite";
 	    }
 	  }
-	 
+}
+/*after click the icon, the screen goes down and plane audio */
+function one() {
+	window.scrollTo(0, 1572);
+	var audio = document.getElementById("plane");
+	audio.play();
+
+	
+}
+
+function two() {
+	window.scrollTo(0, 2358);
+	var audio = document.getElementById("plane");
+	audio.pause();
+
+}
+
+function three() {
+	window.scrollTo(0, 3135);	 
 }
 
 function four() {
-	window.scrollTo(0, 4355);
+	window.scrollTo(0, 3910);
 	var audio = document.getElementById("bubble");
 	/*loop the sound of bubbling.*/
 	audio.addEventListener('ended', function() {
@@ -52,7 +54,7 @@ function four() {
 	
 }
 function five() {
-	window.scrollTo(0, 5185);
+	window.scrollTo(0, 4700);
 	var audio = document.getElementById("bubble");
 	audio.pause();
 }
