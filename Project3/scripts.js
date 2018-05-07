a01c5df01b8692e80acf61bbf157d8273225632d
Jize Tao <JasonTao@Jasons-MacBook-Air.local>
2018-05-07 12:04:12 +0400

    5/7

--- Project3/scripts.js
+++ Project3/scripts.js
@@ -1,28 +1,31 @@
 function begin() {
-	window.scrollTo(0, 786);
+	$('html, body').animate({scrollTop: $("#panel-1").offset().top}, 800);	
 }
+
 function back() {
-	window.scrollTo(0, 0);
+	$('html, body').animate({scrollTop: $("#titlePage").offset().top}, 800);
 }
+
 function climb() {
 	var elem = document.getElementById("man");   
-	  var pos = 28.53;
-	  var id = setInterval(frame, 200);
-	  function frame() {
-	    if (pos == 74.53) {
-	      clearInterval(id);
-	      document.getElementById("man").style.animation = "";
-	    } else {
-	      pos++; 
-	      elem.style.left = 0.34*pos + 'vw'; 
-	      elem.style.top = 528.53-50 - pos + 'vh'; 
-	      document.getElementById("man").style.animation = "shake 2s infinite";
-	    }
-	  }
+  	var initPos = 161.9;
+  	var pos = initPos;
+  	var id = setInterval(frame, 200);
+  	function frame() {
+    	if (pos >= initPos + 46) {
+      		clearInterval(id);
+      		document.getElementById("man").style.animation = "";
+    	} else {
+      		pos++; 
+      		elem.style.left = 0.21*pos + 'vw'; 
+      		elem.style.top = 636.9- pos + 'vh'; 
+      		document.getElementById("man").style.animation = "shake 2s infinite";
+    	}
+  	}
 }
 /*after click the icon, the screen goes down and plane audio */
 function one() {
-	window.scrollTo(0, 1572);
+	$('html, body').animate({scrollTop: $("#panel-2").offset().top}, 800);
 	var audio = document.getElementById("plane");
 	audio.play();
 
@@ -30,31 +33,31 @@ function one() {
 }
 
 function two() {
-	window.scrollTo(0, 2358);
+	$('html, body').animate({scrollTop: $("#panel-3").offset().top}, 800);
 	var audio = document.getElementById("plane");
 	audio.pause();
 
 }
 
 function three() {
-	window.scrollTo(0, 3135);	 
+	$('html, body').animate({scrollTop: $("#panel-4").offset().top}, 800);	 
 }
 
 function four() {
-	window.scrollTo(0, 3910);
+	$('html, body').animate({scrollTop: $("#panel-5").offset().top}, 800);
 	var audio = document.getElementById("bubble");
 	/*loop the sound of bubbling.*/
-	audio.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-	}, false);
+	// audio.addEventListener('ended', function() {
+ //    	this.currentTime = 0;
+ //    	this.play();
+	// 	}, false);
 	audio.play();
 
 	/*after clicking the button 4, the man climbing the mountain*/
 	
 }
 function five() {
-	window.scrollTo(0, 4700);
+	$('html, body').animate({scrollTop: $("#panel-6").offset().top}, 800);
 	var audio = document.getElementById("bubble");
 	audio.pause();
 }
